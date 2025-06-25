bfeecab3f99b84dccf1b69f822c68797a6cc4ed0
rururux <rururux@gmail.com>
2025-06-26 01:21:19 +0900

    set page path

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://rururux.github.io",
+  base: "/portfolio",
   vite: {
     plugins: [tailwindcss()]
   }
